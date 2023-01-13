9e64d49baff23ac65f0494715a43ff275ef58397
suryaalla <asvp.0296@gmail.com>
2023-01-13 13:22:20 +0100

    update on 13.01.2023

--- geometry_images.py
+++ geometry_images.py
@@ -2,9 +2,6 @@ import igl
 import meshplot as mp
 import numpy as np
 
-# loading and plotting mesh
+# loading mesh
 vert, tri = igl.read_triangle_mesh('./data/processed_data/0002_letti.obj')
-
-
-
 
